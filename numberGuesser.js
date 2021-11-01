08d6544a57cdd8b1e5d8f95e29cd382ca59095ba
SalomeYaaAmanfo <92305202+SalomeYaaAmanfo@users.noreply.github.com>
2021-11-01 19:34:39 +0000

    Add files via upload

---
+++ numberGuesser.js
@@ -0,0 +1,42 @@
+// Write your code below:
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+function generateTarget(){
+  let secretTarget = Math.floor(Math.random()*9)
+  return secretTarget
+}
+function compareGuesses(userGuess,computerGuess,secretTarget){
+  
+  if( userGuess == computerGuess){
+        return true;
+  }
+  if(userGuess != computerGuess){
+    userGuess = Math.abs(userGuess - secretTarget)
+    computerGuess = Math.abs(computerGuess - secretTarget)
+  if(userGuess < computerGuess){
+    return true ;
+  }
+  if(computerGuess < userGuess){
+    return false ;
+  }
+ }
+}
+
+let winner = '';
+function updateScore(winner){
+  switch(winner){
+    case 'human':
+    humanScore+=1
+    break;
+    case 'computer':
+    computerScore+=1
+    break;
+  }
+}
+function advanceRound(){
+  currentRoundNumber += 1
+}
+
+
